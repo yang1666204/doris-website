856d41925b8ae572d4133380d05e3ca513f35ee8
liyang <liyang@selectdb.com>
2024-12-16 17:40:33 +0800

    test

--- tools/modify_mdx_checker.js
+++ tools/modify_mdx_checker.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
-const constantsContent = fs.readFileSync(path.join(__dirname, '../docusaurus-mdx-checker/src/constants.js'), 'utf-8');
-const result = constantsContent.replaceAll('**/*.{md,mdx}', '**/*.{mdx}');
+const constantsContent = fs.readFileSync(path.join(__dirname, '../docusaurus-mdx-checker/src/main.js'), 'utf-8');
+const result = constantsContent.replaceAll('format = "mdx"', 'format = "detect"');
 
-fs.writeFileSync(path.join(__dirname, '../docusaurus-mdx-checker/src/constants.js'), result, 'utf-8');
+fs.writeFileSync(path.join(__dirname, '../docusaurus-mdx-checker/src/main.js'), result, 'utf-8');
+
